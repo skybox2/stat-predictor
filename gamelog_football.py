e8a3ea4c72d6c83d36528b58c4b210dc071c8716
vivekhebbar <vhebbar13@gmail.com>
2016-10-16 18:31:31 -0700

    changed it to use argparse and changed the nicknames

--- gamelog_football.py
+++ gamelog_football.py
@@ -10,60 +10,6 @@ access_token = "d49525fb16260a10a902f32b33aaa172"
 offensive_positions = ['QB', 'RB', 'WR', 'TE']
 current_season = "nfl-2016-2017"
 
-
-# This function builds the roster table, which contains record entries
-# for all current players of all teams in the form:
-# (first name, last name, team slug, position, player slug, player id)
-# def populateRoster():
-# 	# Query stattleship for NFL teams
-# 	new_query = Stattleship()
-# 	token = new_query.set_token(access_token)
-# 	teams = new_query.ss_get_results(sport='football',league='nfl',ep='teams')
-# 	# Construct dictionary of all team_slugs and each matching team_id
-# 	team_slugs = {item['slug'] : item['id'] for item in teams[0]['teams']}
-# 	# Open DB connection and create roster table
-# 	conn = sqlite3.connect('nfl.db')
-# 	c = conn.cursor()
-# 	c.execute('''CREATE TABLE IF NOT EXISTS roster (first text, last text,
-# 		team text, position text, player_slug text, player_id text PRIMARY KEY)''')
-# 	conn.commit()
-# 	# Loop through each team in team_slugs
-# 	for team in team_slugs:
-# 		# player_records will contain all of the team's player records to be inserted
-# 		# into roster at end of for loop
-# 		player_records = []
-# 		# Want to look at all valid pages of players for a given team
-# 		for index in range(1, 30):
-# 			# Query stattleship for a page of team's roster
-# 			page = new_query.ss_get_results(sport='football',league='nfl', \
-# 			ep='players', team_id=team, season_id=current_season, page=str(index))
-# 			page_of_players = page[0]['players']
-# 			# If there are no more players to consider, move to next team
-# 			if not page_of_players:
-# 				break
-# 			# Loop through each player on page
-# 			for player in page_of_players:
-# 				# If player should be considered, add his record to be inserted
-# 				if player['position_abbreviation'] in offensive_positions:
-# 					record = (player['first_name'], player['last_name'], \
-# 						team, player['position_abbreviation'], player['slug'], \
-# 						player['id'])
-# 					player_records += [record]
-# 		# Create one record for team's D/ST
-# 		defense = (team, 'D/ST', team, 'D/ST', team, team_slugs[team])
-# 		player_records += [defense]
-# 		# Insert team's player records, commit
-# 		sql_stmt = "INSERT OR REPLACE INTO roster VALUES (?, ?, ?, ?, ?, ?)"
-# 		c.executemany(sql_stmt, player_records)
-# 		conn.commit()
-# 		print "Done with team " + team + ". "
-# 	# Compute the number of records added within for loops
-# 	c.execute("SELECT COUNT(*) FROM roster")
-# 	num_records = c.fetchone()[0]
-# 	print "There are " + str(num_records) + " records in the roster table."
-# 	# Upon looping through all teams, close DBconnection
-# 	conn.close()
-
 # This function builds the gamelog table, which contains record entries
 # for all game performances in the past year for all players in the form:
 # (player id, , , , , )
@@ -100,7 +46,7 @@ def populateGameLog():
 # gamelog table that has been populated with populateGameLog(), and
 # that this function solely exists to incrementally add recent logs 
 # to an existing log table.
-def updateGameLog(date, conn=None):
+def updateGameLog(date=datetime.now(), conn=None):
 	# Generate cursor for database connection if c=None
 	if not conn:
 		conn = sqlite3.connect('nfl.db')
